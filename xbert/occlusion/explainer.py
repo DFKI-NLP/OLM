Fix: attention mask not used in batcher - refactored Explainer so that arbitrary tensors can be passed as parameters to the model

xbert/occlusion/explainer.py
@@ -4,38 +4,42 @@ import torch
 
 # explainers taken from: https://github.com/yulongwang12/visual-attribution/blob/master/explainer/backprop.py
 
+
 class VanillaGradExplainer:
-    def __init__(self, model, output_getter=None):
+    def __init__(self, model, input_key, output_getter=None):
         self.model = model
+        self.input_key = input_key
         self.output_getter = output_getter
 
     def _backprop(self, inp, ind):
-        output = self.model(inputs_embeds=inp)
+        output = self.model(**inp)
         if self.output_getter is not None:
             output = self.output_getter(output)
         grad_out = output.data.clone()
         grad_out.fill_(0.0)
         grad_out.scatter_(1, ind.unsqueeze(0).t(), 1.0)
         output.backward(grad_out)
-        return inp.grad.data
+        return inp[self.input_key].grad.data
 
     def explain(self, inp, ind):
         return self._backprop(inp, ind)
 
 
 class GradxInputExplainer(VanillaGradExplainer):
-    def __init__(self, model, output_getter=None):
+    def __init__(self, model, input_key=None, output_getter=None):
         super().__init__(model=model,
+                         input_key=input_key,
                          output_getter=output_getter)
 
     def explain(self, inp, ind):
         grad = self._backprop(inp, ind)
-        return inp.data * grad
-    
-    
+        return inp[self.input_key].data * grad
+
+
 class SaliencyExplainer(VanillaGradExplainer):
-    def __init__(self, model, output_getter=None):
+    def __init__(self, model, input_key=None, output_getter=None):
         super().__init__(model=model,
+                         input_key=input_key,
                          output_getter=output_getter)
 
     def explain(self, inp, ind):
@@ -44,17 +48,19 @@ class SaliencyExplainer(VanillaGradExplainer):
 
 
 class IntegrateGradExplainer(VanillaGradExplainer):
-    def __init__(self, model, steps=100, output_getter=None):
+    def __init__(self, model, steps=100, input_key=None, output_getter=None):
         super().__init__(model=model,
+                         input_key=input_key,
                          output_getter=output_getter)
         self.steps = steps
 
     def explain(self, inp, ind):
         grad = 0
-        inp_data = inp.data.clone()
+        inp_data = inp[self.input_key].data.clone()
 
         for alpha in np.arange(1 / self.steps, 1.0, 1 / self.steps):
-            new_inp = Variable(inp_data * alpha, requires_grad=True)
+            new_inp = inp.copy()
+            new_inp[self.input_key] = Variable(inp_data * alpha, requires_grad=True)
             g = self._backprop(new_inp, ind)
             grad += g
 